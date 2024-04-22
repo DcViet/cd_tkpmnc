21a707c34bd708621896cb239c712e316b35eb57
DcViet <111166640+DcViet@users.noreply.github.com>
2024-04-22 11:02:13 +0000

    feat: new

--- backEnd/services/userService.js
+++ backEnd/services/userService.js
@@ -11,4 +11,16 @@ class DriverService {
     }
 }
 
-module.exports = DriverService;
+
+class CustomerService {
+    constructor() {
+        // Khởi tạo các biến cần thiết cho dịch vụ khach hang
+    }
+
+    getNearbyDrivers(passengerLocation) {
+        // Triển khai để tìm các tài xế gần đây dựa trên vị trí của hành khách
+    }
+}
+
+
+module.exports = { DriverService, CustomerService };
